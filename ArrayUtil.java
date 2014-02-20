48204e442af48f7d9ccc64681af8da14a7c46665
rasmus03 <rasmus_275@hotmail.com>
2014-02-20 14:29:17 +0100

    good*

--- ArrayUtil.java
+++ ArrayUtil.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 /**
- * Dette er et fucking HAHAHAHAHHAHAHAHAHAHAAHAHHAHa god luck.
+ * Dette er et fucking HAHAHAHAHHAHAHAHAHAHAAHAHHAHa good luck.
  */
 
 public static class ArrayUtil {
