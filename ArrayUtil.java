4905124ffc4f428dc804e7ab218b94218d3d4c54
rasmus03 <rasmus_275@hotmail.com>
2014-02-20 14:08:26 +0100

    snydt

--- ArrayUtil.java
+++ ArrayUtil.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
 /**
- * Dette er et stenet java program 
+ * Dette er et fucking fedt java program!!
  */
 
 public class ArrayUtil {
-	private static Random generator = new Random();
+	private Random generator = new Random();
 
-	public static int[] randomIntArray(int length, int n) {
+	public int[] randomIntArray(int length, int n) {
 		int[] a = new int[length];
 		for (int i = 0; i < a.length; i++) {
 			a[i] = generator.nextInt(n);
